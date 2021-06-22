d6acec5e3b60ac91455351ac607df838f4d6fa1a
SENTHILRAJ1976 <41649243+SENTHILRAJ1976@users.noreply.github.com>
2021-06-22 14:03:09 +0530

    Add files via upload

---
+++ TC001_Login.java
@@ -0,0 +1,28 @@
+package testcases;
+
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import wdMethods.SeMethods;
+
+public class TC001_Login extends SeMethods{
+	@Test
+	public void login() {
+		startApp("chrome", "http://leaftaps.com/opentaps");
+		WebElement eleUserName = locateElement("id", "username1");
+		type(eleUserName, "DemoSalesManager");
+		WebElement elePassword = locateElement("id","password");
+		type(elePassword, "crmsfa");
+		WebElement eleLogin = locateElement("class","decorativeSubmit");
+		click(eleLogin);
+	}
+
+}
+
+
+
+
+
+
+
+
